77db33fb172641c4c81dea033da4022c20bfbec7
Maksim Kostromin <daggerok@gmail.com>
2019-04-07 20:22:30 +0300

    Add withWarmer example.

--- src/main/scala/com/github/daggerok/App.scala
+++ src/main/scala/com/github/daggerok/App.scala
@@ -6,11 +6,19 @@ object App {
   def main(args: Array[String]): Unit = {
     val from = if (args.length < 1 || null == args(0)) 0 else args(0).toInt
     val to = if (args.length < 2 || null == args(1)) 1000000 else args(1).toInt
+
     val time = measure {
       (from until to).toArray.reverse
     }
     println(s"""
             |executed "($from until $to).toArray.reverse" in: $time
+            |""".stripMargin)
+
+    val withWarmerTime = withWarmer(new Warmer.Default) measure {
+      (from until to).toArray.reverse
+    }
+    println(s"""
+            |executed with warmer "($from until $to).toArray.reverse" in: $withWarmerTime
             |""".stripMargin)
   }
 }
